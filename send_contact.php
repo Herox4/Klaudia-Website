bde011b3ab306d3d96b7181e0f1ff187a7e8beca
Herox4 <dennis.thomiczny@gmx.net>
2024-09-08 18:27:57 +0200

    Add files via upload

---
+++ send_contact.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Formulardaten abrufen
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // E-Mail-Adresse des Empfängers
+    $to = "deine-email@example.com";
+    $subject = "Neue Kontaktanfrage von $name";
+    $body = "Name: $name\nE-Mail: $email\n\nNachricht:\n$message";
+    $headers = "From: $email";
+
+    // E-Mail senden
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Ihre Nachricht wurde gesendet.";
+    } else {
+        echo "Es gab ein Problem beim Senden Ihrer Nachricht.";
+    }
+}
+?>
